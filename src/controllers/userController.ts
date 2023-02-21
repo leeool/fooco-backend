461511cf31db0b1cf6f9663dc9fe34f6984922b5
leeool <lgonsalez02@gmail.com>
2023-02-21 19:43:47 -0300

    ✨ feat: update userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,37 @@
 import { Request, Response } from "express"
+import postRepository from "../repositories/postRepository"
 import userRepository from "../repositories/userRepository"
 
 class UserController {
-  async create(req: Request, res: Response) {
+  async index(req: Request, res: Response) {
+    const users = await userRepository.find({ relations: { posts: true } })
+
+    res.status(200).json(users)
+  }
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params
+
+    const user = await userRepository.findOne({
+      relations: { posts: true },
+      where: { id: id }
+    })
+
+    if (!user) {
+      return res.status(400).json({ error: "User not found" })
+    }
+
+    res.json({ user })
+  }
+
+  async store(req: Request, res: Response) {
     // criar usuário
     const { username, email, password } = req.body
 
     if (!username || !email || !password) {
       return res
         .status(400)
-        .json({ message: "Por favor, preencha todos os campos." })
+        .json({ error: "Por favor, preencha todos os campos." })
     }
 
     const newUser = userRepository.create({
@@ -19,6 +41,11 @@ class UserController {
     })
     await userRepository.save(newUser)
     res.status(201).json(newUser)
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params
+    const { username, email, password } = req.body
   }
 }
 

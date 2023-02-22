795d1f27f652bebd3af2eb31d5cd495d912e8f77
leeool <lgonsalez02@gmail.com>
2023-02-22 00:48:04 -0300

    👽️ feat: change code to production

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,7 @@ import { DataSource } from "typeorm"
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  url: process.env.DB_URL,
   entities: [__dirname + "/**/entities/*{.ts,.js}"],
   migrations: [__dirname + "/**/migrations/*{.ts,.js}"]
 })

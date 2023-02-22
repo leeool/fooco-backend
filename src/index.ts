d3dd311a4dd256b25ae1e4fbc2d2d5a5ebde51f6
leeool <lgonsalez02@gmail.com>
2023-02-22 15:20:50 -0300

    ✨ feat: add error middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Express from "express"
 import cors from "cors"
 import router from "./routes"
 import AppDataSource from "./data-source"
+import errorMiddleware from "./middlewares/error"
 
 AppDataSource.initialize().then(() => {
   const app = Express()
@@ -9,6 +10,8 @@ AppDataSource.initialize().then(() => {
   app.use(Express.json())
   app.use(cors())
   app.use(router)
+
+  app.use(errorMiddleware)
 
   app.listen(process.env.PORT, () => {
     console.log("🔥 Server is running at http://localhost:" + process.env.PORT)
---
+++ src/middlewares/error.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express"
+import ApiError from "../helpers/apiErrors"
+
+const errorMiddleware = (
+  err: Error & Partial<ApiError>,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const message = err.statusCode
+    ? err.message
+    : "Foquinho encontrou um problema."
+  const statusCode = err.statusCode || 500
+
+  return res.status(statusCode).json({ error: message })
+}
+
+export default errorMiddleware

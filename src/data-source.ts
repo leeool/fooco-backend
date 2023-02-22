6dc8b365682b42d869015a40924d6d957ca0ed56
leeool <lgonsalez02@gmail.com>
2023-02-22 01:36:04 -0300

    ✨ feat: conect database to aws server

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,14 @@ import { DataSource } from "typeorm"
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DB_URL,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
   entities: [__dirname + "/**/entities/*{.ts,.js}"],
   migrations: [__dirname + "/**/migrations/*{.ts,.js}"]
 })

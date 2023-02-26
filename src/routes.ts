b9026baa04ca57870af8d05c7592fca29b93258e
leeool <lgonsalez02@gmail.com>
2023-02-26 00:52:02 -0300

    ✨ feat: add schemas parser and limiters

--- src/routes.ts
+++ src/routes.ts
@@ -1,54 +1,61 @@
 import { Router } from "express"
+import "express-async-errors"
 import postController from "./controllers/postController"
 import userController from "./controllers/userController"
-import { auth } from "./middlewares/auth"
-import "express-async-errors"
-import schemaParse from "./middlewares/schemaParse"
-import { validUUID, userSchema, postSchema } from "./schemas/"
 import tokenController from "./controllers/tokenController"
+import setLimiter from "./middlewares/rateLimiter"
+import auth from "./middlewares/auth"
+import schemaParse from "./middlewares/schemaParse"
+import {
+  postSchema,
+  schemaUpdateUser,
+  schemaGetUser,
+  schemaDeleteUser,
+  schemaCreateUser
+} from "./schemas/"
+import {
+  deleteUserLimit,
+  loginUserLimit,
+  postUserLimit,
+  updateUserLimit
+} from "./helpers/limiters"
 
 const router = Router()
 
 // POSTS
 router.get("/post", postController.index)
-router.get("/post/:postId", schemaParse(validUUID), postController.show)
-router.post(
-  "/post",
-  schemaParse(validUUID),
-  schemaParse(postSchema),
-  auth,
-  postController.store
-)
+router.get("/post/:postId", postController.show)
+router.post("/post", schemaParse(postSchema), auth, postController.store)
 router.put(
   "/post/:postId",
-  schemaParse(validUUID),
   schemaParse(postSchema),
   auth,
   postController.update
 )
-router.delete(
-  "/post/:postId",
-  schemaParse(validUUID),
-  auth,
-  postController.delete
-)
+router.delete("/post/:postId", auth, postController.delete)
 
 // USERS
 router.get("/user", userController.index)
-router.get("/user/:userId", schemaParse(validUUID), userController.show)
-router.post("/user", schemaParse(userSchema), userController.store)
-router.post("/user/login", userController.login)
+router.get("/user/:user_id", schemaParse(schemaGetUser), userController.show)
+router.post(
+  "/user",
+  setLimiter(postUserLimit),
+  schemaParse(schemaCreateUser),
+  userController.store
+)
+router.post("/user/login", setLimiter(loginUserLimit), userController.login)
 router.put(
-  "/user/:userId",
-  schemaParse(validUUID),
-  schemaParse(userSchema),
+  "/user/:user_id",
+  setLimiter(updateUserLimit),
+  schemaParse(schemaUpdateUser),
   auth,
   userController.update
 )
 router.delete(
-  "/user/:userId",
-  schemaParse(validUUID),
+  "/user/:user_id",
+  setLimiter(deleteUserLimit),
   auth,
+  schemaParse(schemaDeleteUser),
   userController.delete
 )
 

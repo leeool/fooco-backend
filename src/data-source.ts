b3a7881677477ec1b62506bdf0825c2f36a9da80
leeool <lgonsalez02@gmail.com>
2023-02-26 02:43:37 -0300

    🌐 fix: add ssl

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,9 +10,9 @@ const AppDataSource = new DataSource({
   // username: process.env.DB_USER,
   // password: process.env.DB_PASS,
   // database: process.env.DB_NAME,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // },
+  ssl: {
+    rejectUnauthorized: false
+  },
   entities: [__dirname + "/**/entities/*{.ts,.js}"],
   migrations: [__dirname + "/**/migrations/*{.ts,.js}"]
 })

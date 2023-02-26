d0ded07cf43081f8ef8b3dc3040db5536689c5dc
leeool <lgonsalez02@gmail.com>
2023-02-26 00:51:18 -0300

    🔒️ feat: add dafault limiter and update cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import cors from "cors"
 import router from "./routes"
 import AppDataSource from "./data-source"
 import errorMiddleware from "./middlewares/error"
+import setLimiter from "./middlewares/rateLimiter"
+import { defaultLimiter } from "./helpers/limiters"
 
 AppDataSource.initialize().then(() => {
   const app = Express()
 
   app.use(Express.json())
-  app.use(cors({ credentials: true, origin: "https://fooco.netlify.app" }))
+  app.use(cors({ credentials: true, origin: true }))
+  app.use(setLimiter(defaultLimiter))
   app.use(router)
   app.use(errorMiddleware)
   app.listen(process.env.PORT, () => {

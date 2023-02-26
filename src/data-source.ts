9453ef848c9ea698af2fd2f8de1653c9a05759c4
leeool <lgonsalez02@gmail.com>
2023-02-26 03:02:06 -0300

    🌐 fix: update db connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,12 +4,12 @@ import { DataSource } from "typeorm"
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DB_URL,
-  // host: process.env.DB_HOST,
-  // port: Number(process.env.DB_PORT),
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // database: process.env.DB_NAME,
+  // url: process.env.DB_URL,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false
   },

1a405558e823ef8c585bc03d9e8484861ae87caf
leeool <lgonsalez02@gmail.com>
2023-02-26 02:41:09 -0300

    🌐 feat: change db server

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,14 +4,15 @@ import { DataSource } from "typeorm"
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  },
+  url: process.env.DB_URL,
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT),
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
+  // ssl: {
+  //   rejectUnauthorized: false
+  // },
   entities: [__dirname + "/**/entities/*{.ts,.js}"],
   migrations: [__dirname + "/**/migrations/*{.ts,.js}"]
 })

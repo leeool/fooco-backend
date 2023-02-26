3c6090c1de3a0cd376fcb6a8692b4a4770987b17
leeool <lgonsalez02@gmail.com>
2023-02-26 00:43:09 -0300

    🔊 feat: add error logs

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express"
+import logController from "../controllers/logController"
 import ApiError from "../helpers/apiErrors"
+import logRepository from "../repositories/logRepository"
 
 const errorMiddleware = (
   err: Error & Partial<ApiError>,
@@ -11,7 +13,27 @@ const errorMiddleware = (
     ? err.message
     : "Foquinho encontrou um problema );"
   const statusCode = err.statusCode || 500
-  console.log(err.message)
+
+  if (statusCode === 429) {
+    req.log = logRepository.create({
+      message: err.message,
+      statusCode: err.statusCode,
+      method: req.method,
+      path: req.path,
+      ip: req.ip,
+      logDate: new Date()
+    })
+
+    logController.store(req, res)
+  }
+
+  console.log({
+    message: err.message,
+    statusCode: err.statusCode,
+    method: req.method,
+    path: req.path
+  })
+
   return res.status(statusCode).json({ error: message })
 }
 

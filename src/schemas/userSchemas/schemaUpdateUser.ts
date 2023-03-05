f8bd69b29233bc0efcdfaa6abf86b9dedbfed835
leeool <lgonsalez02@gmail.com>
2023-03-05 18:11:52 -0300

    ✨ feat: update schema update user

--- src/schemas/userSchemas/schemaUpdateUser.ts
+++ src/schemas/userSchemas/schemaUpdateUser.ts
@@ -22,8 +22,14 @@ const schemaUpdateUser = z.object({
       .string()
       .trim()
       .min(8, "A Senha deve ter no mínimo 8 caracteres")
-      .regex(regexAtLeastOneNumber, "A senha deve conter números e letras")
-      .optional()
+      .regex(passwordRegex, "A senha deve conter números e letras")
+      .optional(),
+    tags: z.array(z.string()).optional(),
+    educational_place: z.string().optional(),
+    educational_place_url: z.string().url("Formato de URL inválida").optional(),
+    about: z.string().max(100).optional(),
+    avatar_url: z.string().url("Formato de URL Inválido").optional(),
+    banner_url: z.string().url("Formato de URL inválido").optional()
   }),
   params: z.object({
     user_id: z.string().uuid("ID de usuário inválido")

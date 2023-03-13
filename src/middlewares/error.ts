8f23e1810ce97907361bc1e04e6fd9e7e618b0bf
leeool <lgonsalez02@gmail.com>
2023-03-13 10:06:23 -0300

    🔥 feat: remove log feature

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from "express"
-import logController from "../controllers/logController"
 import ApiError from "../helpers/apiErrors"
-import logRepository from "../repositories/logRepository"
 
 const errorMiddleware = (
   err: Error & Partial<ApiError>,
@@ -13,19 +11,6 @@ const errorMiddleware = (
     ? err.message
     : "Foquinho encontrou um problema );"
   const statusCode = err.statusCode || 500
-
-  if (statusCode === 429) {
-    req.log = logRepository.create({
-      message: err.message,
-      statusCode: err.statusCode,
-      method: req.method,
-      path: req.path,
-      ip: req.ip,
-      logDate: new Date()
-    })
-
-    logController.store(req, res)
-  }
 
   console.log({
     message: err.message,
--- src/repositories/logRepository.ts
+++
@@ -1,6 +0,0 @@
-import AppDataSource from "../data-source"
-import Log from "../entities/Log"
-
-const logRepository = AppDataSource.getRepository(Log)
-
-export default logRepository

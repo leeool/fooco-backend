10c2fafd2bb74e923de99d5c56c47331c6dc988e
leeool <lgonsalez02@gmail.com>
2023-04-15 15:59:19 -0300

    ✨ feat: token validation return user data

--- src/controllers/tokenController.ts
+++ src/controllers/tokenController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import { UnauthorizedError } from "../helpers/apiErrors"
 import jwt from "jsonwebtoken"
+import userRepository from "../repositories/userRepository"
 
 class tokenController {
   validadeToken(req: Request, res: Response) {
@@ -12,9 +13,15 @@ class tokenController {
 
     const token = authorization.split(" ")[1]
 
-    jwt.verify(token, process.env.JWT_PASS!, (err, decoded) => {
-      if (err) throw new UnauthorizedError("Token inválido.")
-      return res.status(200).json(decoded)
+    jwt.verify(token, process.env.JWT_PASS!, async (err, decoded) => {
+      if (err || !decoded || typeof decoded === "string" || !("id" in decoded))
+        throw new UnauthorizedError("Token inválido.")
+
+      const user = await userRepository.findOneBy({ id: decoded.id })
+
+      console.log(user)
+
+      return res.status(200).json({ decoded, user })
     })
   }
 }
